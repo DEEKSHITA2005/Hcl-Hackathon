c82cdd3004d577365aca718e64e2acd5cd5e4450
Leela2200031423 <2200031423@kluniversity.in>
2026-04-08 13:17:16 +0530

    Patient Model

--- HealthSync/src/main/java/com/hcl/HealthSync/repository/PatientRepository.java
+++ HealthSync/src/main/java/com/hcl/HealthSync/repository/PatientRepository.java
@@ -1,4 +1,10 @@
 package com.hcl.HealthSync.repository;
 
-public interface PatientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hcl.HealthSync.model.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+	
+	
 }
